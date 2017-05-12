4739f9f668cadd88179e3355ecf5d1fe1feac50e
TheLaMarty <martinaalevin@gmail.com>
2017-05-12 13:29:43 +0200

    Initial commit

---
+++ src/main/java/com/m/controllers/Controller.java
@@ -0,0 +1,93 @@
+package com.m.controllers;
+
+import com.m.beans.Contact;
+import com.m.repository.ContactService;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import java.util.List;
+
+@ManagedBean(name = "controller")
+public class Controller {
+
+    private List<Contact> contacts;
+    private List<Contact> searchResult;
+    private Contact newContact = new Contact();
+    private int idDelete;
+    private String searchString;
+    private String message;
+
+    @ManagedProperty("#{service}")
+    private ContactService contactService;
+
+    public void setContactService(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @PostConstruct
+    public void init() {
+        contacts = contactService.getContacts();
+    }
+
+    public Contact getNewContact() {
+        return newContact;
+    }
+
+    public void setNewContact(Contact newContact) {
+        this.newContact = newContact;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void createContact() {
+        contactService.addContact(newContact);
+
+    }
+
+    public void searchContact(){
+        contactService.searchContact(searchString);
+    }
+
+    public void deleteContact() {
+        contactService.deleteContact(idDelete);
+    }
+
+    public int getIdDelete() {
+        return idDelete;
+    }
+
+    public void setIdDelete(int idDelete) {
+        this.idDelete = idDelete;
+    }
+
+    public String getSearchString() {
+        return searchString;
+    }
+
+    public void setSearchString(String searchString) {
+        this.searchString = searchString;
+    }
+
+    public String getMessage() {
+
+        message = contactService.getMessage();
+
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<Contact> getSearchResult() {
+        searchResult = contactService.getSearchResult();
+        return searchResult;
+    }
+
+    public void setSearchResult(List<Contact> searchResult) {
+        this.searchResult = searchResult;
+    }
+}
---
+++ src/main/java/com/m/repository/ContactService.java
@@ -0,0 +1,84 @@
+package com.m.repository;
+
+import com.m.beans.Contact;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
+
+@Singleton
+@ApplicationScoped
+@ManagedBean(name = "service")
+public class ContactService {
+
+    private List<Contact> contacts = new ArrayList<>();
+    private List<Contact> searchResult = new ArrayList<>();
+    private int id = 1;
+    private String message;
+
+    public void addContact(Contact newContact) {
+        newContact.setId(id++);
+        contacts.add(newContact);
+    }
+
+    public void deleteContact(int idDelete) {
+        for (int i = 0; i < contacts.size(); i++) {
+
+            if (contacts.get(i).getId() == idDelete) {
+                contacts.remove(i);
+            } else {
+                message = "Contact with id " + idDelete + " does not exist";
+            }
+        }
+    }
+
+    public void searchContact(String searchString) {
+
+        for (int i = 0; i < contacts.size(); i++) {
+
+            if (contacts.get(i).getFirstName().equals(searchString) || contacts.get(i).getLastName().equals(searchString) || contacts.get(i).getCategory().equals(searchString) || contacts.get(i).getEmail().equals(searchString) || contacts.get(i).getPhone().equals(searchString) && contacts.get(i).getFirstName() != null) {
+
+                searchResult.add(contacts.get(i));
+            }
+
+/*            } else {
+                message = "No contact found";
+            }*/
+
+        }
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public List<Contact> getSearchResult() {
+        return searchResult;
+    }
+
+    public void setSearchResult(List<Contact> searchResult) {
+        this.searchResult = searchResult;
+    }
+
+    @PostConstruct
+    public void createDB() {
+
+        addContact(new Contact("Isabel", "Franzen", "isabel.franzen@bonniernews.com", "0735956852", "Work"));
+        addContact(new Contact("Emelie", "Welander", "emelie.welander@bonniernews.com", "0735976832", "Work"));
+        addContact(new Contact("Sofie", "Jensen", "sofie.jensen@bonniernews.com", "0730576894", "Work"));
+        addContact(new Contact("Anna", "Jandler", "anna.jandler@bonniernews.com", "0765576743", "Work"));
+    }
+
+}
